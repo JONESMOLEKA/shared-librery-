92890879ec5e28da9c1d8be3579c312f363403a1
f1suppa <112776537+f1suppa@users.noreply.github.com>
2022-10-10 09:20:36 -0400

    Update var maths.groovy

--- sharelibrary/var maths.groovy
+++ sharelibrary/var maths.groovy
@@ -5,11 +5,11 @@ echo "the sum of $(a) and $(b) is $(a+b)"
 def subtract (a,b)
 {
 echo "the subrtaction of $(a) and $(b) is $(a-b)"
-}
+} Jonas has a 
 deg mul (a,b)
 {
 echo "the product of $(a) and $(b) is $(a*b)"
-}
+  
 def div(a,b)
 {
 the division of $(a) and $(b) is $(a/b)"
